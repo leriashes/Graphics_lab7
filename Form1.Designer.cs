eb9144ca6bd7878979994220a1288862e15ef55e
leriashes <valeriya-shevchenko-2002@mail.ru>
2023-12-16 01:30:33 +0700

    Билинейное сглаживание

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,21 +29,120 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.OpenFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 375);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(77, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 359);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Коэффициент";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Получить результат";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenFileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1129, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // OpenFileToolStripMenuItem
+            // 
+            this.OpenFileToolStripMenuItem.Name = "OpenFileToolStripMenuItem";
+            this.OpenFileToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.OpenFileToolStripMenuItem.Text = "Открыть файл";
+            this.OpenFileToolStripMenuItem.Click += new System.EventHandler(this.открытьФайлToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Graphics_lab7.Properties.Resources.Без_имени_12;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(331, 298);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Graphics_lab7.Properties.Resources.Без_имени_12;
+            this.pictureBox2.Location = new System.Drawing.Point(371, 47);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(331, 298);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1129, 589);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
             this.Text = "Лабораторная работа 7";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem OpenFileToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,144 @@ namespace Graphics_lab7
 {
     public partial class MainForm : Form
     {
+        private double coef = 2;
+        private int k = 1;
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private Bitmap MakeImgWithBordersCopy(PictureBox picture, int bordersize)
+        {
+            Bitmap imgStart = new Bitmap(picture.Image);
+
+            Bitmap img = new Bitmap(imgStart.Width + bordersize * 2, imgStart.Height + bordersize * 2);
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    int x, y;
+
+                    if (i < bordersize)
+                    {
+                        x = 0;
+                    }
+                    else if (i >= bordersize + imgStart.Width)
+                    {
+                        x = imgStart.Width - 1;
+                    }
+                    else
+                    {
+                        x = i - bordersize;
+                    }
+
+                    if (j < bordersize)
+                    {
+                        y = 0;
+                    }
+                    else if (j >= bordersize + imgStart.Height)
+                    {
+                        y = imgStart.Height - 1;
+                    }
+                    else
+                    {
+                        y = j - bordersize;
+                    }
+
+                    img.SetPixel(i, j, imgStart.GetPixel(x, y));
+                }
+            }
+
+            return img;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            coef = Convert.ToDouble(textBox1.Text);
+
+            Bitmap imgStart = MakeImgWithBordersCopy(pictureBox1, 1);
+
+            int halfsize = 25;
+            int size = halfsize * 2;
+            double hs = halfsize / coef;
+            double s = hs * 2;
+
+
+            Bitmap imgRes = new Bitmap(Convert.ToInt32(Math.Round(pictureBox1.Image.Width * coef)), Convert.ToInt32(Math.Round(pictureBox1.Image.Height * coef)));
+
+            for (int i = 0; i < imgRes.Width; i++) 
+            {
+                double x = (i * 2 + 1) * hs;
+
+                int sx = Convert.ToInt32(Math.Truncate(x / halfsize));
+
+                if (sx % 2 == 0)
+                {
+                    sx -= 1;
+                }
+
+                sx *= halfsize;
+
+                int si = (sx / halfsize + 1) / 2;
+
+                for (int j = 0; j < imgRes.Height; j++)
+                {
+                    double y = (j * 2 + 1) * hs;
+
+                    int sy = Convert.ToInt32(Math.Truncate(y / halfsize));
+
+                    if (sy % 2 == 0)
+                    {
+                        sy -= 1;
+                    }
+
+                    sy *= halfsize;
+
+                    int sj = (sy / halfsize + 1) / 2;
+
+
+                    Color pixel11 = imgStart.GetPixel(si, sj);
+                    Color pixel12 = imgStart.GetPixel(si, sj + 1);
+                    Color pixel21 = imgStart.GetPixel(si + 1, sj);
+                    Color pixel22 = imgStart.GetPixel(si + 1, sj + 1);
+
+
+
+                    int R, G, B;
+
+                    double f1 = (sx + size - x) / size * pixel11.R + (x - sx) / size * pixel21.R;
+                    double f2 = (sx + size - x) / size * pixel12.R + (x - sx) / size * pixel22.R;
+
+                    R = Convert.ToInt32(Math.Round((sy + size - y) / size * f1 + (y - sy) / size * f2));
+
+                    f1 = (sx + size - x) / size * pixel11.G + (x - sx) / size * pixel21.G;
+                    f2 = (sx + size - x) / size * pixel12.G + (x - sx) / size * pixel22.G;
+
+                    G = Convert.ToInt32(Math.Round((sy + size - y) / size * f1 + (y - sy) / size * f2));
+
+                    f1 = (sx + size - x) / size * pixel11.B + (x - sx) / size * pixel21.B;
+                    f2 = (sx + size - x) / size * pixel12.B + (x - sx) / size * pixel22.B;
+
+                    B = Convert.ToInt32(Math.Round((sy + size - y) / size * f1 + (y - sy) / size * f2));
+
+                    imgRes.SetPixel(i, j, Color.FromArgb(255, R, G, B));
+                }
+            }
+
+            string st = k++ + ".png";
+
+            imgRes.Save(st, ImageFormat.Png);
+            pictureBox2.Image = imgRes;
+        }
+
+        private void открытьФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(openFileDialog1.FileName);
+            }
         }
     }
 }
